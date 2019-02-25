cc66017c41f8b32e9a07b9b676a4c110b710a107
gaki2745 <guyeongjun@naver.com>
2019-02-25 21:24:03 +0900

    Singleton

---
+++ Singleton/SingletonTests/SingletonTests.swift
@@ -0,0 +1,34 @@
+//
+//  SingletonTests.swift
+//  SingletonTests
+//
+//  Created by youngjun goo on 25/02/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import XCTest
+@testable import Singleton
+
+class SingletonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
