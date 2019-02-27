1ef47094a02931885e348faf8fa0af415cb2171c
gaki2745 <guyeongjun@naver.com>
2019-02-28 00:50:08 +0900

    Escaping 예제 추가

--- Closure_HighOrderFunc_Exam/Closure_HighOrderFunc_Exam/main.swift
+++ Closure_HighOrderFunc_Exam/Closure_HighOrderFunc_Exam/main.swift
@@ -8,5 +8,73 @@
 
 import Foundation
 
-print("Hello, World!")
+//let names: [String] = ["x", "c", "b", "a", "k"]
+//
+//func backwards(first: String, second: String) -> Bool {
+//    print("\(first) \(second) 비교중")
+//    return first < second
+//}
+//
+//let reversed: [String] = names.sorted(by: backwards)
+//print(reversed)
+//
+//
+//
+//
+//let reversed2: [String] = names.sorted(by: { (first: String, second: String) -> Bool in
+//    return first < second
+//})
+//
+//print(reversed2)
 
+
+
+// 함수 외부에 클로저를 저장하는 예시
+// 클로저를 저장하는 배열
+var completionHandlers: [() -> Void] = []
+
+func withEscaping(completion: @escaping () -> Void) {
+    // 함수 밖에 있는 completionHandlers 배열에 해당 클로저를 저장
+    completionHandlers.append(completion)
+}
+
+func withoutEscaping(completion: () -> Void) {
+    completion()
+}
+
+class MyClass {
+    var x = 10
+    func callFunc() {
+        withEscaping { self.x = 100 }
+        withoutEscaping { x = 200 }
+    }
+}
+let mc = MyClass()
+mc.callFunc()
+print(mc.x)
+completionHandlers.first?()
+print(mc.x)
+
+
+class Server {
+    static var users: [UserModel] = []
+    
+    static getUser(completion: @escaping (Bool, [UserModel]) -> Void) {
+        //2
+        Alamofire.request(urlRequest).responseJSON { reponse in
+            users.append(유저)
+            DispatchQueue.main.async {
+                //3
+                completion(true, users)
+            }
+        }
+    }
+}
+
+//1
+Server.getUser{ (isSuccess, users) in
+    //4
+    if isSuccess {
+    //성공했기 때문에 UI Update 작업 수행
+    }
+}
