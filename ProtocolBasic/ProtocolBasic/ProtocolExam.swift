b358f6047f897c1d8f87e62ed7acea7d86f75da8
gaki2745 <guyeongjun@naver.com>
2019-03-01 00:28:36 +0900

    프로토콜 기본 예제추가

---
+++ ProtocolBasic/ProtocolBasic/ProtocolExam.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  ProtocolBasic
+//
+//  Created by youngjun goo on 01/03/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import Foundation
+
+protocol CustomLayout {
+    func layoutSetting()
+}
+
+
---
+++ ProtocolBasic/ProtocolBasic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtocolBasic
+//
+//  Created by youngjun goo on 01/03/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, CustomLayout {
+
+    func layoutSetting() {
+        //dosometing
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
