d5930b187c87448748dea380601e7bd093093af9
gaki2745 <guyeongjun@naver.com>
2019-03-20 00:03:52 +0900

    FMDB 객체 생성 코드 추가

--- DAOPatternExam/DAOPatternExam/DepartmentDAO.swift
+++ DAOPatternExam/DAOPatternExam/DepartmentDAO.swift
@@ -11,5 +11,26 @@ import Foundation
 
 
 class DepartmentDAO {
-    lazy var fmdb:
+    
+    // SQLite 연결 및 초기화
+    lazy var fmdb: FMDatabase! = {
+        // 1. 파일 매니저 객체 생성
+        let fileMgr = FileManager.default
+        
+        // 2. 샌드박스 내 문서 디렉터리에서 데이터베이스 파일 경로를 확인
+        let docPath = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        let dbPath = docPath!.appendingPathComponent("hr.sqlite").path
+        
+        // 3. 샌드박스 경로에 파일이 없다면 메인 번들에 만들어 둔 hr.sqlite를 가져와 복사한다
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "hr", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
+        }
+        
+        // 4. 준비된 데이터베이스 파일을 바탕으로 FMDatabase객체를 생성한다
+        let db = FMDatabase(path: dbPath)
+        return db
+        
+    }()
 }
