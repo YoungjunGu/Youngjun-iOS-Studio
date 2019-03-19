4db42078782b1bec2f14ecfc0c3529a3fd4c3d9a
gaki2745 <guyeongjun@naver.com>
2019-03-19 23:51:48 +0900

    fmdb import

---
+++ DAOPatternExam/DAOPatternExam/DepartmentDAO.swift
@@ -0,0 +1,15 @@
+//
+//  DepartmentDAO.swift
+//  DAOPatternExam
+//
+//  Created by youngjun goo on 19/03/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import Foundation
+
+
+
+class DepartmentDAO {
+    lazy var fmdb:
+}
