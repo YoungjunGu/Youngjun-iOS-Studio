c95e91a0a6a14cd64b628795f19801174d655769
gaki <guyeongjun@naver.com>
2020-05-26 23:24:58 +0900

    SwiftGen Test

---
+++ SwiftGenTest/SwiftGenTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftGenTest
+//
+//  Created by youngjun goo on 2020/05/25.
+//  Copyright © 2020 youngjun goo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
